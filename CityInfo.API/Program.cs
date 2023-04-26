aa97493c9743ccee76cdf89ac664c00088b8608f
David Gómez Redondo <david.gomez.redondo@avanade.com>
2023-04-26 12:02:27 +0200

    authentication middeware

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -3,6 +3,8 @@ using CityInfo.API.DbContexts;
 using Microsoft.AspNetCore.StaticFiles;
 using Serilog;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace CityInfo.API
 {
@@ -52,6 +54,20 @@ namespace CityInfo.API
             builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+            builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Authentication:Issuer"],
+                        ValidAudience = builder.Configuration["Authentication:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Authentication:Secret"]))
+                    };
+                });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -65,6 +81,8 @@ namespace CityInfo.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
